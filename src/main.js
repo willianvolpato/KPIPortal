179b180ce176f48a22eb0d1acb82e9a9c54cbca2
Willian Volpato <willian.volpato@gmail.com>
2018-12-01 14:49:20 -0200

    Init repo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Vuetify from 'vuetify';
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
