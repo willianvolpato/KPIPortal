49ee40869e4f27c31e43b1493c29912b30ab3f41
Willian Volpato <willian.volpato@gmail.com>
2018-12-23 12:27:58 -0200

    Fix list-group

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -9,7 +9,36 @@
       >
         <v-list dense class="sidebarNavigation">
           <template v-for="item in items">
-            <v-list-tile @click="" :key="item.text" :class="item.cssClass ? item.cssClass : ''">
+            <v-list-group
+              v-if="item.children"
+              v-model="item.model"
+              :key="item.text"
+              :prepend-icon="item.model ? item.icon : item['icon-alt']"
+              append-icon=""
+            >
+              <v-list-tile slot="activator">
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ item.text }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile
+                v-for="(child, i) in item.children"
+                :key="i"
+                @click=""
+              >
+                <v-list-tile-action v-if="child.icon">
+                  <v-icon>{{ child.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ child.text }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list-group>
+            <v-list-tile v-else @click="" :key="item.text" :class="item.cssClass ? item.cssClass : ''">
               <v-list-tile-action>
                 <v-icon>{{ item.icon }}</v-icon>
               </v-list-tile-action>
